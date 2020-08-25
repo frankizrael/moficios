bdd88d4cdeac3c9744cfbf60c9920c1c4cca800d
frankizrael <frankdelarosacampos@gmail.com>
2020-08-25 17:15:16 -0500

    fix moficios

--- wp-content/themes/moficios/src/app.js
+++ wp-content/themes/moficios/src/app.js
@@ -3,7 +3,7 @@ import './scss/app.scss';
 
 // Import scripts
 import 'bootstrap/js/src/dropdown';
-
+/*
 $('.oficio').on('click',function(){
 	$('.header_contact').addClass('active');
 	$('.header_contact__bull').addClass('active');
@@ -12,4 +12,4 @@ $('.oficio').on('click',function(){
 $('.header_contact__bull').on('click',function(){
 	$('.header_contact').removeClass('active');
 	$('.header_contact__bull').removeClass('active');
-});
+});*/
